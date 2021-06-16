c6c76cfd97b48ec72ffee3dba252415d319c17ae
chris <chris@LAPTOP-U94663UT>
2021-06-17 01:39:34 +0800

    update

--- src/main/java/com/TDDPOMSeleniumFramework/datautil/Common.java
+++ src/main/java/com/TDDPOMSeleniumFramework/datautil/Common.java
@@ -6,7 +6,7 @@ public class Common {
 	public static final String PATH_SCREENSHOTS = System.getProperty("user.dir") + "/src/test/resources/screenshot/";
 	public static final String PATH_WEBDRIVERS = "./src/test/resources/webdrivers/";
 	public static final String PATH_DOWNLOADS = "./src/test/resources/downloads/";
-	public static final String PATH_REPORT = "./target/reports/";
+	public static final String PATH_REPORT = "./reports/";
 	public static final String LOG_PROPERTIES_FILE = "./src/test/resources/config/log4j.properties";
 	public static final String REFERENCE_VALUE_MODULE1 = "Automation Test";
 	public static final String REFERENCE_VALUE_LOGIN = "ENVIRONMENT";
